2c8ad86c8b262ffd25a18d80efd4d56869d7c81f
EFFECT <info@effect.moe>
2023-08-16 02:53:47 +0900

    re

--- next.config.js
+++ next.config.js
@@ -20,3 +20,16 @@ module.exports = withBundleAnalyzer({
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
 })
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+}
+
+module.exports = withPWA(nextConfig)
